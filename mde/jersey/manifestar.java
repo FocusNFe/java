6905ddec740a313b62d49b2e3d67ce0183cb72b2
marceloleite-acras <marcelo.leite@acras.com.br>
2018-10-04 16:28:58 -0300

    Java - Manivestar - corrigido nome variavel

--- mde/jersey/manifestar.java
+++ mde/jersey/manifestar.java
@@ -12,24 +12,24 @@ public class Manifestar {
 	public static void main(String[] args) {
 		
 		String login = "Token_enviado_pelo_Suporte";
-		String chave = "Chave_de_identificação_da_NFe";
+		String chave = "Chave_de_identificaï¿½ï¿½o_da_NFe";
 		
-		/* Para ambiente de produção use a variável abaixo:
+		/* Para ambiente de produï¿½ï¿½o use a variï¿½vel abaixo:
 		String server = "https://api.focusnfe.com.br/"; */
 		String server = "http://homologacao.acrasnfe.acras.com.br/";
 		String url = server.concat("v2/nfes_recebidas/"+chave+"/manifesto");
 			
 		/* Aqui criamos um hashmap para receber a chave "tipo" e o valor que pode ser: ciencia, confirmacao, desconhecimento ou nao_realizada. */		
 		HashMap<String, String> tipoManifestacao = new HashMap<String, String>();
-		TipoManifestacao.put("tipo", "nao_realizada");
+		tipoManifestacao.put("tipo", "nao_realizada");
 		
-		/* Caso escolha o tipo "nao_realizada", é preciso informar o campo/chave "justificativa".
-		 * TipoManifestacao.put("justificativa", "Informe aqui a sua justificativa do motivo da não realização da operação."); */
+		/* Caso escolha o tipo "nao_realizada", ï¿½ preciso informar o campo/chave "justificativa".
+		 * TipoManifestacao.put("justificativa", "Informe aqui a sua justificativa do motivo da nï¿½o realizaï¿½ï¿½o da operaï¿½ï¿½o."); */
 			
 		/* Criamos um objeto JSON para receber a hash com os dados esperado pela API. */
 		JSONObject json = new JSONObject(TipoManifestacao);
 		
-		/* Configuração para realizar o HTTP BasicAuth. */
+		/* Configuraï¿½ï¿½o para realizar o HTTP BasicAuth. */
 		Object config = new DefaultClientConfig();
 		Client client = Client.create((ClientConfig) config);
 		client.addFilter(new HTTPBasicAuthFilter(login, ""));
@@ -39,8 +39,8 @@ public class Manifestar {
 		int httpCode = resposta.getStatus(); 
 		String body = resposta.getEntity(String.class);
 		
-		/* As três linhas abaixo imprimem as informações retornadas pela API. 
-		 * Aqui o seu sistema deverá interpretar e lidar com o retorno. */
+		/* As trï¿½s linhas abaixo imprimem as informaï¿½ï¿½es retornadas pela API. 
+		 * Aqui o seu sistema deverï¿½ interpretar e lidar com o retorno. */
 		System.out.print("HTTP Code: ");
 		System.out.print(httpCode);
 		System.out.printf(body);
